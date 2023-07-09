4384171ae35b8236d48abf0a4328c8cd302d3225
Chacko jithin <jithinchackopayyanat@gmail.com>
2023-07-10 01:53:12 +0530

    updateProfile function

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -2,16 +2,23 @@ import React, { useState,useContext } from 'react';
 import Logo from '../../olx-logo.png';
 import './Signup.css';
 import FirebaseContext from '../../store/firebaseContext';
+import { getAuth, createUserWithEmailAndPassword,updateProfile } from "firebase/auth";
 
 export default function Signup() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [password, setPassword] = useState('');
-  const {db} = useContext(FirebaseContext)
+  const {db} = useContext(FirebaseContext);
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(db)
+    e.preventDefault();
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password).then((res)=>{
+      updateProfile(res.user, {
+        displayName:username
+      })
+      console.log(res.user)
+    })
    };
 
   return (
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
-
-
+import 'firebase/auth'
+import 'firebase/firestore'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCrwjPHY3LcPNPVD-Y4Kbkm0x7n0eYxmxQ",
